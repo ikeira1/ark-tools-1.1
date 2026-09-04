0302a11e594ce00e8e2ae304c42df554d01d9665
ikeira1 <alshra7ili@outlook.com>
2026-09-04 04:00:41 +0300

    Add files via upload

--- analytics.js
+++ analytics.js
@@ -1,18 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore, doc, updateDoc, increment, onSnapshot, setDoc, getDoc, deleteDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyD27a-WhhKdvblKUduPrZkBcmUr-uYkED4",
-    authDomain: "ark-tools.firebaseapp.com",
-    projectId: "ark-tools",
-    storageBucket: "ark-tools.firebasestorage.app",
-    messagingSenderId: "576361005916",
-    appId: "1:576361005916:web:4df24f62da976ae2cdb799",
-    measurementId: "G-4LD2YGH0TW"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { db } from "./firebase-config.js";
+import { doc, updateDoc, increment, onSnapshot, setDoc, getDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 // تاريخ اليوم بصيغة YYYY-MM-DD
 function getTodayString() {
